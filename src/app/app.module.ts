436085f35598f178f3579de5b21a9f420c328844
Tybo Vanderstraeten <tybo.vanderstraeten@student.hogent.be>
2019-04-06 13:12:07 +0200

    Add Google Maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Components
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { EuropeMapComponent } from './components/europe-map/europe-map.component';
+
+//Modules
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatToolbarModule,MatCardModule } from '@angular/material';
-import {MatIconModule} from '@angular/material/icon';
-import { FooterComponent } from './footer/footer.component';
-import { EuropeMapComponent } from './europe-map/europe-map.component';
+import { MatButtonModule, MatToolbarModule, MatCardModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -22,9 +26,17 @@ import { EuropeMapComponent } from './europe-map/europe-map.component';
     MatButtonModule,
     MatToolbarModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBxI2mg6yieter_1y-tj5SY4s9sbHjhARo'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  title: string = 'PsyGarden europe';
+  lat: Number = 39.571625;
+  lng: Number = 2.650544;
+  zoom: Number = 15;
+}
--- src/app/components/europe-map/europe-map.component.ts
+++ src/app/components/europe-map/europe-map.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-europe-map',
   templateUrl: './europe-map.component.html',
-  styleUrls: ['./europe-map.component.css']
+  styleUrls: [
+    './europe-map.component.css']
 })
 export class EuropeMapComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
