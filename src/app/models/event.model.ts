c359386df26b3ab05355c62bf4b80ad653ee673e
Tybo Vanderstraeten <tybo.vanderstraeten@student.hogent.be>
2019-04-22 15:00:53 +0200

    Change resource to link

--- src/app/models/event.model.ts
+++ src/app/models/event.model.ts
@@ -14,7 +14,7 @@ export class Event {
         private _zipCode: string,
         private _genres: any[],
         private _prices: any[],
-        private _resources: any[]
+        private _links: any[]
     ) { }
 
     get eventId(): Number { return this._eventId; }
@@ -31,11 +31,11 @@ export class Event {
     get zipCode(): string { return this._zipCode; }
     get genres(): any[] { return this._genres; }
     get prices(): any[] { return this._prices; }
-    get resources(): any[] { return this._resources; }
+    get links(): any[] { return this._links; }
 
     static fromJSON(json: any): Event {
         const eventFromJSON = new Event(json.eventId, json.name, json.description, json.startDate, json.endDate,
-            json.nrOfDays, json.country, json.region, json.city, json.street, json.streetNr, json.zipCode, json.eventGenres, json.prices, json.resources
+            json.nrOfDays, json.country, json.region, json.city, json.street, json.streetNr, json.zipCode, json.eventGenres, json.prices, json.links
         );
         return eventFromJSON;
     }
