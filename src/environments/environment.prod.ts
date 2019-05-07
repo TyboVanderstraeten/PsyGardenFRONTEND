ec34315aa2cbed7e466d3114348a7b5843c2dfb1
Tybo Vanderstraeten <tybo.vanderstraeten@student.hogent.be>
2019-05-07 13:21:45 +0200

    Add deletethis in key to not get my money stolen by gOoGuL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   psyGardenApiUrl: '/PsyGardenAPI',
-  googleApiKey: 'AIzaSyCqt3qllbndhWdL3TMSYtv_krlf4YHUrRw'
+  googleApiKey: 'DELETETHISAIzaSyCqt3qllbndhWdL3TMSYtv_krlf4YHUrRw'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   psyGardenApiUrl: '/PsyGardenAPI',
-  googleApiKey: 'AIzaSyCqt3qllbndhWdL3TMSYtv_krlf4YHUrRw'
+  googleApiKey: 'DELETETHISAIzaSyCqt3qllbndhWdL3TMSYtv_krlf4YHUrRw'
 };
