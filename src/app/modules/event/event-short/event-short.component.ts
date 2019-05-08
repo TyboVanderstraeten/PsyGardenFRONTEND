7d1d34db043b97b82ad748a5cf80b5b3f3418907
Tybo Vanderstraeten <tybo.vanderstraeten@student.hogent.be>
2019-05-08 14:56:54 +0200

    All events styling

--- src/app/modules/event/event-short/event-short.component.ts
+++ src/app/modules/event/event-short/event-short.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Event } from 'src/app/modules/event/event.model';
-import { UserDataService } from '../../user/user-data.service';
 
 @Component({
   selector: 'app-event-short',
@@ -10,20 +9,9 @@ import { UserDataService } from '../../user/user-data.service';
 export class EventShortComponent implements OnInit {
   @Input() event: Event;
 
-  constructor(
-    private _userDataService: UserDataService) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  addToInterested(eventId: Number) {
-    this._userDataService.addToInterested(eventId)
-      .subscribe();
-  }
-
-  addToGoing(eventId: Number) {
-    this._userDataService.addToGoing(eventId)
-      .subscribe();
   }
 
 }
