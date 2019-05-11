e2c858d7eac258156aff00acfc395938687995a7
Tybo Vanderstraeten <tybo.vanderstraeten@student.hogent.be>
2019-05-11 19:02:53 +0200

    Change in register test

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -11,14 +11,15 @@ describe('Register testing', function () {
     cy.get('[data-cy=passwordConfirmation]');
   });
   it('Registers', function () {
+    const random = Math.round(Math.random(5000)*100000);
     cy.visit('http://localhost:4200/register');
     cy.get('[data-cy=firstname]').type('Test');
     cy.get('[data-cy=lastname]').type('Test');
-    cy.get('[data-cy=email]').type('test@test.com');
-    cy.get('[data-cy=emailConfirmation]').type('test@test.com');
+    cy.get('[data-cy=email]').type(`test${random}@test.com`);
+    cy.get('[data-cy=emailConfirmation]').type(`test${random}@test.com`);
     cy.get('[data-cy=password]').type('P@ssword1');
     cy.get('[data-cy=passwordConfirmation]').type('P@ssword1');
     cy.get('[data-cy=registerButton]').click();
-    cy.contains('test@test.com');
+    cy.contains(`test${random}@test.com`);
   });
 });
