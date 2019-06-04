7cbb078fe70d6d1f0e3d8c5ebe4e63ff018cfea6
Tybo Vanderstraeten <tybo.vanderstraeten@student.hogent.be>
2019-06-04 10:12:04 +0200

    Add checking for geocoding call in authinterceptor

--- src/app/interceptors/AuthenticationInterceptor.ts
+++ src/app/interceptors/AuthenticationInterceptor.ts
@@ -16,6 +16,9 @@ export class AuthenticationInterceptor implements HttpInterceptor {
         req: HttpRequest<any>,
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
+        if (req.url.startsWith('https://maps.googleapis.com/maps/api/geocode/')) {
+            return next.handle(req);
+        }
         if (this._authenticationService.token.length) {
             const clonedRequest = req.clone({
                 headers: req.headers.set(
